45c2327536e22ffefb98b551c992fdaae743be2e
Michael Gomes <michael.gomes@etu.hesge.ch>
2020-02-25 21:02:44 +0100

    Mise en place de la methode "PassagePossible"

--- src/metier/PyRat.java
+++ src/metier/PyRat.java
@@ -20,9 +20,9 @@ public class PyRat {
         Point pt2 = new Point(3,1);
         System.out.println((fromageIci(pt2) ? "Il y a un" : "Il n'y a pas de") + " fromage ici, en position " + pt2);
         System.out.println((fromageIci_EnOrdreConstant(pt2) ? "Il y a un" : "Il n'y a pas de") + " fromage ici, en position " + pt2);
-        System.out.println((passagePossible(pt1, pt2) ? "Il y a un" : "Il n'y a pas de") + " passage de " + pt1 + " vers " + pt2);
+        System.out.println((passagePossible(pt1, pt2, laby) ? "Il y a un" : "Il n'y a pas de") + " passage de " + pt1 + " vers " + pt2);
         System.out.println((passagePossible_EnOrdreConstant(pt1, pt2) ? "Il y a un" : "Il n'y a pas de") + " passage de " + pt1 + " vers " + pt2);
-        System.out.println("Liste des points inatteignables depuis la position " + position + " : " + pointsInatteignables(position));
+        System.out.println("Liste des points inatteignables depuis la position " + position + " : " + pointsInatteignables(position, laby));
     }
 
     /* Regarde dans la liste des fromages s’il y a un fromage à la position pos.
@@ -68,7 +68,12 @@ public class PyRat {
 
     /* Indique si le joueur peut passer de la position (du Point) « de » au point « a ».
         @return true s'il y a un passage depuis  « de » vers « a ». */
-    private boolean passagePossible(Point de, Point a) {
+    private boolean passagePossible(Point de, Point a, Map<Point, List<Point>> laby) {
+        for (Point el : laby.get(de)){
+            if(el.equals(a)){
+                return true;
+            }
+        }
         return false;
     }
 
@@ -79,9 +84,12 @@ public class PyRat {
         return false;
     }
 
+
     /* Retourne la liste des points qui ne peuvent pas être atteints depuis la position « pos ».
         @return la liste des points qui ne peuvent pas être atteints depuis la position « pos ». */
-    private List<Point> pointsInatteignables(Point pos) {
+    private List<Point> pointsInatteignables(Point pos, Map<Point, List<Point>> laby) {
+        System.out.println(laby);
+
         return null;
     }
 }
