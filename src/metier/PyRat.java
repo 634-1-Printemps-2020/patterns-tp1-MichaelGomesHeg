a9159458c1ae2c8b6cb521833ab47a29c74605f3
Michael Gomes <michael.gomes@etu.hesge.ch>
2020-02-26 14:10:53 +0100

    Mise en place de la methode "PassagePossible_constant"

--- src/metier/PyRat.java
+++ src/metier/PyRat.java
@@ -21,7 +21,7 @@ public class PyRat {
         System.out.println((fromageIci(pt2) ? "Il y a un" : "Il n'y a pas de") + " fromage ici, en position " + pt2);
         System.out.println((fromageIci_EnOrdreConstant(pt2) ? "Il y a un" : "Il n'y a pas de") + " fromage ici, en position " + pt2);
         System.out.println((passagePossible(pt1, pt2, laby) ? "Il y a un" : "Il n'y a pas de") + " passage de " + pt1 + " vers " + pt2);
-        System.out.println((passagePossible_EnOrdreConstant(pt1, pt2) ? "Il y a un" : "Il n'y a pas de") + " passage de " + pt1 + " vers " + pt2);
+        System.out.println((passagePossible_EnOrdreConstant(pt1, pt2, laby) ? "Il y a un" : "Il n'y a pas de") + " passage de " + pt1 + " vers " + pt2);
         System.out.println("Liste des points inatteignables depuis la position " + position + " : " + pointsInatteignables(position, laby));
     }
 
@@ -80,17 +80,32 @@ public class PyRat {
     /* Indique si le joueur peut passer de la position (du Point) « de » au point « a »,
         mais sans devoir parcourir la liste des Points se trouvant dans la Map !
         @return true s'il y a un passage depuis  « de » vers « a ». */
-    private boolean passagePossible_EnOrdreConstant(Point de, Point a) {
+    /* Indique si le joueur peut passer de la position (du Point) « de » au point « a »,
+    mais sans devoir parcourir la liste des Points se trouvant dans la Map !
+    @return true s'il y a un passage depuis  « de » vers « a ». */
+    private boolean passagePossible_EnOrdreConstant(Point de, Point a, Map<Point, List<Point>> laby) {
+        if(collectionMaker(de, a, laby).contains(a)){
+            return true;
+        };
         return false;
     }
+
+    private Set collectionMaker(Point de, Point a, Map<Point, List<Point>> laby){
+        /***
+         * Création de la collection
+         */
+        Set<Point> innerMap = new HashSet<>();
+        for(Point el : laby.get(de)){
+            innerMap.add(el);
+        }
+        return innerMap;
+    }
+
 
 
     /* Retourne la liste des points qui ne peuvent pas être atteints depuis la position « pos ».
         @return la liste des points qui ne peuvent pas être atteints depuis la position « pos ». */
     private List<Point> pointsInatteignables(Point pos, Map<Point, List<Point>> laby) {
-        System.out.println(laby);
-
-
-        return null;
+         return null;
     }
 }
